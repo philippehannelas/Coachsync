33f5dac33bd01699258fd0d4cc18de2f39e581c3
philippehannelas <phannelas@gmail.com>
2025-10-28 11:42:33 +0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -14,6 +14,8 @@ from src.routes.booking import booking_bp
 from src.routes.availability import availability_bp
 from src.routes.date_specific_availability import date_specific_bp
 from src.routes.migrate import migrate_bp
+from src.routes.migrate import migrate_events_bp
+
 
 app = Flask(__name__, static_folder=os.path.join(os.path.dirname(__file__), 'static'))
 app.config['SECRET_KEY'] = 'asdf#FGSgvasgf$5$WGT'
@@ -30,6 +32,7 @@ app.register_blueprint(booking_bp, url_prefix='/api')
 app.register_blueprint(availability_bp, url_prefix='/api')
 app.register_blueprint(date_specific_bp, url_prefix='/api')
 app.register_blueprint(migrate_bp, url_prefix='/api')
+app.register_blueprint(migrate_events_bp, url_prefix='/api')
 
 # Database configuration for production
 # Use PostgreSQL if DATABASE_URL is set, otherwise fall back to SQLite
