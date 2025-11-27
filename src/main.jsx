dc9fcc6d9550dda93a44d3146a62ca4d784476a2
philippehannelas <phannelas@gmail.com>
2025-11-27 16:19:44 +0400

    merged index

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import './athletehub-theme.css'
 import App from './App.jsx'
 import { AuthProvider } from './hooks/useAuth.jsx'
 
