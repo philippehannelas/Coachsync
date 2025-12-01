1eca099998b8c5e5b2c08ba94a0bc657705b5421
philippehannelas <phannelas@gmail.com>
2025-12-01 13:21:33 +0400

    Update workout_completion.py

--- src/models/workout_completion.py
+++ src/models/workout_completion.py
@@ -6,8 +6,8 @@ class WorkoutCompletion(db.Model):
     __tablename__ = 'workout_completions'
     
     id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
-    customer_id = db.Column(db.String(36), db.ForeignKey('users.id'), nullable=False)
-    training_plan_id = db.Column(db.String(36), db.ForeignKey('training_plans.id'), nullable=False)
+    customer_id = db.Column(db.String(36), db.ForeignKey('user.id'), nullable=False)
+    training_plan_id = db.Column(db.String(36), db.ForeignKey('training_plan.id'), nullable=False)
     day_number = db.Column(db.Integer, nullable=False)
     completed_at = db.Column(db.DateTime, default=datetime.utcnow)
     duration_minutes = db.Column(db.Integer)  # How long the workout took
@@ -38,7 +38,7 @@ class ExerciseCompletion(db.Model):
     
     id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
     workout_completion_id = db.Column(db.String(36), db.ForeignKey('workout_completions.id'), nullable=False)
-    exercise_id = db.Column(db.String(36), db.ForeignKey('exercises.id'), nullable=False)
+    exercise_id = db.Column(db.String(36), db.ForeignKey('exercise.id'), nullable=False)
     sets_completed = db.Column(db.Integer, default=0)
     reps_completed = db.Column(db.String(100))  # e.g., "10,12,10" for 3 sets
     weight_used = db.Column(db.String(100))  # e.g., "50,50,55" for 3 sets with different weights
